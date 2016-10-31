4641b266f385e7b0dbe772382f2cc981b4a64136
msmilkoff <msmilkoff@gmail.com>
2016-10-31 16:09:56 +0200

    Remove towns

---
+++ DB-Advanced-Entity-Framework/Exercises/IntroductionToEF/13.RemoveTowns/Startup.cs
@@ -0,0 +1,38 @@
+﻿namespace _13.RemoveTowns
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using Data;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var db = new SoftuniContext();
+
+            using (db)
+            {
+                string townName = Console.ReadLine();
+                var townToDelete = db.Towns
+                    .Include(t => t.Addresses)
+                    .FirstOrDefault(t => t.Name == townName);
+
+                var adressesToDelete = townToDelete.Addresses;
+                foreach (var adress in adressesToDelete)
+                {
+                    foreach (var employee in adress.Employees)
+                    {
+                        employee.AddressID = null;
+                    }
+                }
+
+                db.Addresses.RemoveRange(adressesToDelete);
+                db.Towns.Remove(townToDelete);
+                db.SaveChanges();
+
+                Console.WriteLine($"{adressesToDelete.Count} address in {townName} were deleted");
+            }
+        }
+    }
+}
