c07b461fec86982cf6d44a2e9786b692c4839159
msmilkoff <msmilkoff@gmail.com>
2016-10-31 15:41:41 +0200

    Solve 'Increase salaries by department'

---
+++ DB-Advanced-Entity-Framework/Exercises/IntroductionToEF/12.IncreaseSalaries/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace _12.IncreaseSalaries
+{
+    using System;
+    using System.Linq;
+    using Data;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var db = new SoftuniContext();
+
+            var deps = new[] { "Engineering", "Tool Design", "Marketing", "Information Services" };
+
+            using (db)
+            {
+                var employees = db.Employees
+                    .Where(e => deps.Contains(e.Department.Name));
+
+                foreach (var employee in employees)
+                {
+                    employee.Salary *= 1.12m;
+                }
+
+                db.SaveChanges();
+
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine($"{employee.FirstName} {employee.LastName} (${employee.Salary:F6})");
+                }
+            }
+        }
+    }
+}
