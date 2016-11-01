637b9f7dccd11d9c1e818ae2b4f5f8046de81f19
msmilkoff <msmilkoff@gmail.com>
2016-11-01 13:12:04 +0200

    Solve 'FirstLetter'

---
+++ DB-Advanced-Entity-Framework/Exercises/IntroductionToEF/15.FirstLetter/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace _15.FirstLetter
+{
+    using System;
+    using System.Linq;
+    using GringottsData;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var db = new GringottsContext();
+
+            using (db)
+            {
+                var wizardNames = db.WizzardDeposits
+                    .Where(w => w.DepositGroup == "Troll Chest")
+                    .OrderBy(w => w.FirstName)
+                    .Select(w => w.FirstName.Substring(0, 1))
+                    .Distinct();
+
+                foreach (var name in wizardNames)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
