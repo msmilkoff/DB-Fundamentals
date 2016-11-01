e6abb2c369d20f1b951e15129f9020369e17f5fc
msmilkoff <msmilkoff@gmail.com>
2016-11-01 13:10:11 +0200

    Add 'Employees by first name' problem solution

---
+++ DB-Advanced-Entity-Framework/Exercises/IntroductionToEF/14.EmployeesByFirstName/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace _14.EmployeesByFirstName
+{
+    using System;
+    using System.Linq;
+    using Data;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var db = new SoftuniContext();
+
+            using (db)
+            {
+                var employees = db.Employees
+                    .Where(e => e.FirstName.ToLower().StartsWith("sa"))
+                    .Select(e => new
+                    {
+                        FirstName = e.FirstName,
+                        LastName = e.LastName,
+                        JobTitle = e.JobTitle,
+                        Salary = e.Salary
+                    });
+
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine($"{employee.FirstName} {employee.LastName} – {employee.JobTitle} - (${employee.Salary})");
+                }
+            }
+        }
+    }
+}
